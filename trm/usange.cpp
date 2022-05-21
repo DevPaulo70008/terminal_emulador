68c12794591ab54695095da8719e992aed52eada
@DevPaulo28 <102810087+DevPaulo70008@users.noreply.github.com>
2022-05-21 10:32:26 -0300

    Create usange.cpp

---
+++ trm/usange.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <"mai.c">
+/*
+
+
+Esse software esta sobre licença MIT voce pode modificar ou redistribuir uma cópia de sua altoria 
+Pode haver bugs ou travamentos ou total fechamento desse programa 
+
+*/
+
+using namespace std;
+int main()
+{
+
+//nada de loops aqui ou instruções apenas chamada do CPP por esse arquivo 
+
+
+
+}
