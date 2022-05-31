78cff5d8830c552ad5747c17cb281f061bf4e0e9
@DevPaulo28 <102810087+DevPaulo70008@users.noreply.github.com>
2022-05-30 22:00:32 -0300

    Update data.h

--- trm/shlg/data.h
+++ trm/shlg/data.h
@@ -1,1 +1,17 @@
-char terminus=(int*usage; int*chroot());
+#include <stdio.h>
+
+//leia o arquivo de licença incluindo 
+
+/*
+
+Esse software deve receber atualizações já que e um projeto aberto 
+Terminus e um software que acessa o shell de qualquer sistema operacional 
+tanto dispositiveis X86 quando ARM 
+
+
+
+*/
+
+#define _DATA_
+
+void terminus();
