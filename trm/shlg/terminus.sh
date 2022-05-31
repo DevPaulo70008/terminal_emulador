d725a8c789d9290ac88770b87f29251b36d1e65d
@DevPaulo28 <102810087+DevPaulo70008@users.noreply.github.com>
2022-05-30 21:54:22 -0300

    Add files via upload

---
+++ trm/shlg/terminus.sh
@@ -0,0 +1,20 @@
+#!/system/bin/sh 
+vermelhoClaro="\033[1;31m"
+verdeClaro="\033[1;32m"
+amarelo="\033[1;33m"
+
+echo $vermelhoClaro
+echo "*************"
+echo "* ==     == *"
+echo "* @       @ *"
+echo "*     ∆     *-------------->"
+echo "*  -------  *${verdeClaro}💚TERMINUS------->"
+echo "*           *-------->"
+echo "*************"
+echo "*..*  Π  *..*"
+echo "*  *******  *"
+echo "*  *     *  *"
+echo "*  *     *  *"
+echo "._._.   ._._."
+echo $amarelo
+echo "powered By: 🎯DevPaulo©"
